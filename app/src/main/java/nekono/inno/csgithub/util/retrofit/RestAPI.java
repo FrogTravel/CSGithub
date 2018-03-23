0f2e94bab9405e00bb2a76168344a5a6018cb447
FrogTravel <e.levchenko@innopolis.ru>
2018-03-24 02:13:21 +0300

    Comments and refactoring

--- app/src/main/java/nekono/inno/csgithub/util/retrofit/RestAPI.java
+++ app/src/main/java/nekono/inno/csgithub/util/retrofit/RestAPI.java
@@ -8,12 +8,12 @@ import retrofit2.http.GET;
 
 /**
  * Created by ekaterina on 3/22/18.
- * https://api.github.com
- * https://api.github.com/repos/ReactiveX/RxJava/issues/5929
- * https://api.github.com/users/$USER/repos?per_page=100
  */
 
 public interface RestAPI {
+    /**
+     * @return the array of objects with all information about issue
+     */
     @GET("/repos/ReactiveX/RxJava/issues?per_page=100")
     Observable<List<Issue>> getIssues();
 }
